16515670a3420db43813a09bada835e931038ac9
ziad <ziadelnady555@gmail.com>
2020-06-14 13:20:06 -0700

    init app

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  xyz-app
+//
+//  Created by Ziad Ahmed Said Ahmed on 6/14/20.
+//  Copyright © 2020 Ziad Ahmed Said Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
